195eac02b441d38c505e6217fca7b06459188334
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-10-15 13:51:07 +0700

    update app

--- src/app.js
+++ src/app.js
@@ -1,40 +1,41 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const routes = require("./routes");
-const mongoose = require("mongoose");
-const config = require("./config");
-const Logger = require("./middleware/Logger");
+const cors = require('cors');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const config = require('./config');
+const Logger = require('./middleware/Logger');
 const db = mongoose.connection;
 
 //Global middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", config.ENABLE_URL);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
+    res.header('Access-Control-Allow-Origin', config.ENABLE_URL);
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
 });
 app.use(Logger);
 
 //Database connect
 mongoose.connect(config.DB_URL, { useNewUrlParser: true });
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("database connected in " + config.MODE);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('database connected in ' + config.MODE);
 });
 
 //Route configure
 //Enable prefight method
-app.options("*", cors({ origin: config.ENABLE_URL }));
+app.options('*', cors({ origin: config.ENABLE_URL }));
 //Default index route
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get('/', (req, res) => {
+    res.send('hello world');
 });
 
 //Set route
-app.use("/api", routes);
+app.use('/api', routes);
+app.use('/res', express.static(config.UPLOAD_FOLDER));
 
 module.exports = app;
