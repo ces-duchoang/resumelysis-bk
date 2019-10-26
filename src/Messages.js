638a5758558f33eff0bef4ec296c3e831a2fe6e2
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-10-26 13:53:39 +0700

    update message

--- src/Messages.js
+++ src/Messages.js
@@ -1,8 +1,9 @@
 module.exports = {
-  PageOverflowMsg: "Bad request: page index is overflow",
-  DeleteSuccessMsg: "Delete action successfully",
-  UpdateSuccessMsg: "Update action successfully",
-  EmailExistedMsg: "This email is used",
-  SomethingWentWrongMsg: "Something went wrong, please try again",
-  WrongEmailPasswordMsg: "Sorry you input wrong email or password"
+    PageOverflowMsg: 'Bad request: page index is overflow',
+    DeleteSuccessMsg: 'Delete action successfully',
+    UpdateSuccessMsg: 'Update action successfully',
+    EmailExistedMsg: 'This email is used',
+    SomethingWentWrongMsg: 'Something went wrong, please try again',
+    WrongEmailPasswordMsg: 'Sorry you input wrong email or password',
+    ExistResumeMsg: 'This resume already imported'
 };
