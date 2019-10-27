78070d235e205173ee6027fc6833324f99651e30
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-10-27 13:53:55 +0700

    Update MidWare.js

--- src/middleware/MidWare.js
+++ src/middleware/MidWare.js
@@ -1,14 +1,31 @@
-const CheckToken = require("./CheckToken");
-const BodyParser = require("body-parser");
-const AccessFilter = require("./AccessFilter");
-
+const CheckToken = require('./CheckToken');
+const BodyParser = require('body-parser');
+const AccessFilter = require('./AccessFilter');
+const multer = require('multer');
+const config = require('../config');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, config.UPLOAD_FOLDER);
+    },
+    filename: function(req, file, cb) {
+        if (
+            file.originalname.endsWith('.docx') ||
+            file.originalname.endsWith('.pdf')
+        )
+            cb(null, Date.now() + '-' + file.originalname);
+        else cb('File not match requirements', null);
+    }
+});
+const upload = multer({ storage: storage });
 module.exports = {
-  JsonWithAuthMw: [BodyParser.json(), CheckToken],
-  JsonMw: [BodyParser.json()],
-  AuthMw: [CheckToken],
-  JsonWithAuthorization: pers => [
-    BodyParser.json(),
-    CheckToken,
-    AccessFilter(pers)
-  ]
+    JsonWithAuthMw: [BodyParser.json({ limit: "100mb" }), CheckToken],
+    JsonMw: [BodyParser.json({ limit: "100mb" })],
+    AuthMw: [CheckToken],
+    JsonWithAuthorization: pers => [
+        BodyParser.json(),
+        CheckToken,
+        AccessFilter(pers)
+    ],
+    UpFileMw: upload.single('file'),
+    FileWithAuthMw: [CheckToken, upload.single('file')]
 };
