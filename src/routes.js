ca4855078e1912b571dbf0323464fe82046e2448
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-10-29 13:54:49 +0700

    add route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,15 @@
-const router = require("express")();
+const router = require('express')();
 
-router.use("/auth", require("./controllers/auth"));
-router.use("/users", require("./controllers/user"));
+router.use('/auth', require('./controllers/auth'));
+router.use('/users', require('./controllers/user'));
+router.use('/files', require('./controllers/file'));
+router.use('/status', require('./controllers/status'));
+router.use('/majors', require('./controllers/major'));
+router.use('/schools', require('./controllers/school'));
+router.use('/skills', require('./controllers/skill'));
+router.use('/positions', require('./controllers/position'));
+router.use('/degrees', require('./controllers/degree'));
+router.use('/resumes', require('./controllers/resume'));
+router.use('/system', require('./controllers/system'));
 
 module.exports = router;
